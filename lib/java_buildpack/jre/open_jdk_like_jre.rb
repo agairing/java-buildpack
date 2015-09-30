87591382f6c6b48b0368d63eca7887022c2180ad
Andreas Gairing <ag@experts-in-motion.de>
2015-09-30 20:18:57 +0200

    test

--- lib/java_buildpack/jre/open_jdk_like_jre.rb
+++ lib/java_buildpack/jre/open_jdk_like_jre.rb
@@ -54,8 +54,7 @@ module JavaBuildpack
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
         @droplet.java_opts
-          .add_system_property('java.io.tmpdir', '$TMPDIR')
-          .push('-XX:+HeapDumpOnOutOfMemoryError')
+          .add_system_property('java.io.tmpdir', '$TMPDIR')          
           .add_option('-XX:HeapDumpPath', '$PWD/oom_heapdump.hprof')
           .add_option('-XX:OnOutOfMemoryError', killjava)
       end
